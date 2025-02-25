0dd96e7f45e0d738964efa6dec293bd332aa949d
Heribert Schütz <hs@webxcerpt.com>
2025-02-25 13:46:43 +0100

    minor code polishing

--- lookup-account-id.js
+++ lookup-account-id.js
@@ -9,13 +9,13 @@ function lookupAccountId(event) {
     parts.shift();
   }
   if (parts.length !== 2) {
-    output.textContent = 'unexpected account syntax: ' + value;
+    output.value = `Unexpected account syntax: "${value}"`;
     return;
   }
   const [username, instance] = parts;
   (async function() {
     try {
-      output.textContent = `contacting ${instance} ...`;
+      output.value = `Contacting ${instance} ...`;
       const response = await fetch(
       `https://${
         encodeURIComponent(instance)
@@ -23,20 +23,16 @@ function lookupAccountId(event) {
         encodeURIComponent(username)
       }`);
     if (!response.ok) {
-      output.textContent =
-        `could not retrieve user name "${
-          username
-        }" from "${
-          instance
-        }"`;
+      output.value =
+        `Could not retrieve user name "${username}" from "${instance}"`;
       return;
     }
     const json = await response.json();
     // TODO check if there's an id
-    output.textContent =
+    output.value =
       `Account "${username}" on "${instance}" has id "${json.id}".`;
     } catch (e) {
-      output.textContent = `lookup failed: ${e.message}`;
+      output.value = `Lookup failed: ${e.message}`;
     }
   })();
 }
